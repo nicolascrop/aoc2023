7501cdaaab3633c35695cdce7a9ca3057184896c
Nico <crop.nicolas@gmail.com>
2023-12-23 22:57:14 +0100

    feat: init

--- src/resolvers/advent_18.rs
+++ src/resolvers/advent_18.rs
@@ -28,73 +28,17 @@ fn get_direction_from_part(part: &str) -> Direction {
 
 fn display_map(map: &Map) {
     println!("Map:");
-    let mut sum_in = 0;
     for i in 0..map.len() {
-        let mut is_in = false;
         for j in 0..map[i].len() {
-            let mut next_is_in = is_in;
-            if map[i][j].is_some() && (j == map[i].len() - 1 || !map[i][j + 1].is_some()) {
 
-                // No more # at right
-                let mut sum_rest_down = 0;
-                let mut sum_rest_up = 0;
-                let mut sum_rest_both = 0;
-                let mut previous_dir = "up";
-                for k in j+1..map[i].len() {
-                    if map[i][k].is_some() && (k == map[i].len() - 1 || !map[i][k + 1].is_some()) {
-                        if k == map[i].len() - 1 {
-                            sum_rest_both += 1;
-                            next_is_in = !next_is_in;
-                        } else {
-                            if i > 0 && map[i - 1][k].is_some() {
-                                if i < map.len() - 1 && map[i + 1][k].is_some() {
-                                    sum_rest_both += 1;
-                                    next_is_in = !next_is_in;
-                                } else {
-                                    sum_rest_up += 1;
-                                    if !next_is_in {
-                                        next_is_in = true;
-                                    }
-                                }
-                            } else {
-                                sum_rest_down += 1;
-                                if next_is_in {
-                                    next_is_in = false;
-                                }
-                            }
-                        }
-                    }
-                }
-                if is_in {
-                    is_in = next_is_in == false
-                } else {
-                    is_in = next_is_in == true
-                }
-                // println!("{} {} both {} up {} down {} (next {})", i, j, sum_rest_both, sum_rest_up, sum_rest_down, next_is_in);
-                // is_in = next_is_in == true;
-                // is_in = min(sum_rest_down + sum_rest_both, sum_rest_up + sum_rest_both) % 2 != 0;
-            }
-
-            if map[i][j].is_some() || is_in {
-                sum_in += 1;
-            }
             if map[i][j].is_some() {
-                if next_is_in {
-                    print!("{}", "#".cyan().bold())
-                } else {
-                    print!("{}", "#".red().bold())
-                }
+                print!("{}", "#".red().bold())
             } else {
-                if is_in {
-                    print!("{}", ".".green().bold());
-                } else {
-                    print!(".");
-                }
+                print!(".");
             }
         }
         print!("\n");
     }
-    println!("Sum in {}", sum_in);
 }
 
 pub fn resolve(input: &String) {
@@ -103,13 +47,16 @@ pub fn resolve(input: &String) {
     let mut current_point: (usize, usize) = (0, 0);
 
     let mut is_init = true;
+    let mut polygon_vertices: Vec<(usize, usize)> = vec![(0, 0)];
     for line in input.lines() {
         let parts: Vec<&str> = line.split(' ').filter(|x| x.len() > 0).collect();
         let direction = get_direction_from_part(parts[0]);
         let amount: usize = parts[1].parse().unwrap();
         let color: &str = parts[2];
+        let last_vertex = polygon_vertices[polygon_vertices.len() - 1];
 
         if direction == Direction::UP {
+            polygon_vertices.push((last_vertex.0 + amount, last_vertex.1));
             for _ in 0..amount {
                 if current_point.0 == 0 {
                     map.insert(0, vec![None; nb_columns]);
@@ -121,6 +68,7 @@ pub fn resolve(input: &String) {
             }
         }
         if direction == Direction::DOWN {
+            polygon_vertices.push((last_vertex.0 - amount, last_vertex.1));
             for _ in 0..amount {
                 if current_point.0 == map.len() - 1 {
                     map.push(vec![None; nb_columns]);
@@ -133,6 +81,7 @@ pub fn resolve(input: &String) {
             }
         }
         if direction == Direction::LEFT {
+            polygon_vertices.push((last_vertex.0, last_vertex.1 - amount));
             for _ in 0..amount {
                 if current_point.1 == 0 {
                     for row_index in 0..map.len() {
@@ -147,6 +96,7 @@ pub fn resolve(input: &String) {
             }
         }
         if direction == Direction::RIGHT {
+            polygon_vertices.push((last_vertex.0, last_vertex.1 + amount));
             for _ in 0..amount {
                 println!("HH {} {} {}", current_point.1, nb_columns, map.len());
                 if is_init || current_point.1 + 1 == nb_columns {
--- src/resolvers/advent_19.rs
+++ src/resolvers/advent_19.rs
@@ -1,3 +1,247 @@
+use std::collections::HashMap;
+
+const RANGE_MIN: usize = 1;
+const RANGE_MAX: usize = 4000;
+
+#[derive(Hash, Copy, Clone, Debug, Eq, PartialEq)]
+enum Category {
+    X,
+    M,
+    A,
+    S
+}
+
+fn parse_category(ch: char) -> Category {
+    match ch {
+        'x' => Category::X,
+        'm' => Category::M,
+        'a' => Category::A,
+        's' => Category::S,
+        _ => panic!("Category {} non existent", ch)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Operator {
+    LOWER,
+    GREATER
+}
+
+fn parse_operator(ch: char) -> Operator {
+    match ch {
+        '<' => Operator::LOWER,
+        '>' => Operator::GREATER,
+        _ => panic!("Operator {} non existent", ch)
+    }
+}
+
+fn compare(value_a: usize, value_b: usize, op: &Operator) -> bool {
+    return match op {
+        Operator::GREATER => value_a > value_b,
+        Operator::LOWER => value_a < value_b,
+    }
+}
+
+fn get_range_from_operator(value: usize, op: &Operator) -> (usize, usize) {
+    return match op {
+        Operator::GREATER => (value, RANGE_MAX),
+        Operator::LOWER => (RANGE_MIN, value),
+    }
+}
+
+#[derive(Debug)]
+struct Action {
+    category: Option<Category>,
+    operator: Option<Operator>,
+    value_ref: Option<usize>,
+    output: String
+}
+
+impl Action {
+    pub fn new(txt: &str) -> Self {
+        let parts: Vec<&str> = txt.split(':').collect();
+        return if parts.len() == 2 {
+            Self {
+                category: Some(parse_category(parts[0].chars().nth(0).unwrap())),
+                operator: Some(parse_operator(parts[0].chars().nth(1).unwrap())),
+                value_ref: Some(parts[0][2..].to_string().parse().unwrap()),
+                output: String::from(parts[1])
+            }
+        } else {
+            Self {
+                category: None,
+                operator: None,
+                value_ref: None,
+                output: String::from(parts[0])
+            }
+        }
+    }
+
+    pub fn process(&self, values: &Vec<usize>) -> Option<&String> {
+        if !self.category.is_some() {
+            return Some(&self.output);
+        }
+        let index = match self.category.unwrap() {
+            Category::X => 0,
+            Category::M => 1,
+            Category::A => 2,
+            Category::S => 3,
+        };
+        if compare(values[index], self.value_ref.unwrap(), &self.operator.unwrap()) {
+            return Some(&self.output);
+        }
+        return None;
+    }
+
+    pub fn process_range(&self, ranges: &RangeMap) -> (&String, RangeMap, Option<RangeMap>) {
+        if !self.category.is_some() {
+            return (&self.output, ranges.clone(), None);
+        }
+        let category = self.category.unwrap();
+        let mut next_range_valid = ranges.clone();
+        let mut next_range_invalid = ranges.clone();
+        return match self.operator.unwrap() {
+            Operator::GREATER => {
+                let range_value = ranges.get(&category).unwrap();
+                next_range_valid.insert(category, (self.value_ref.unwrap() + 1, range_value.1));
+                next_range_invalid.insert(category, (range_value.0, self.value_ref.unwrap()));
+                (&self.output, next_range_valid.clone(), Some(next_range_invalid.clone()))
+            },
+            Operator::LOWER => {
+                let range_value = ranges.get(&category).unwrap();
+                next_range_valid.insert(category, (range_value.0, self.value_ref.unwrap() - 1));
+                next_range_invalid.insert(category, (self.value_ref.unwrap(), range_value.1));
+                (&self.output, next_range_valid.clone(), Some(next_range_invalid.clone()))
+            }
+        }
+    }
+}
+
+fn do_workflow(rules: &HashMap<String, Vec<Action>>, values: &Vec<usize>) -> bool {
+    let mut current = String::from("in");
+    loop {
+        for action in rules.get(&current).unwrap() {
+            let result = action.process(values);
+            if result.is_some() {
+                current = result.unwrap().clone();
+                break;
+            }
+        }
+
+        if current == "A" {
+            return true;
+        }
+        if current == "R" {
+            return false;
+        }
+    }
+}
+
+type RangeMap = HashMap<Category, (usize, usize)>;
+
+fn compute_range_possibilities(range: &RangeMap) -> usize {
+    let mut result = 0;
+    for r in range {
+        let value = if r.1.0 < r.1.1 { r.1.1 - r.1.0 } else { 0 };
+        if result == 0 {
+            result = value;
+        } else {
+            result *= value;
+        }
+    }
+    return result;
+}
+
+fn explore_workflows(rules: &HashMap<String, Vec<Action>>) {
+    let mut sum = 0;
+    let mut currents: Vec<(String, RangeMap)> = vec![(String::from("in"), HashMap::from([
+        (Category::X, (1, 4000)),
+        (Category::M, (1, 4000)),
+        (Category::A, (1, 4000)),
+        (Category::S, (1, 4000))
+    ]))];
+    loop {
+        let mut next_currents:Vec<(String, RangeMap)> = Vec::new();
+        for current in currents {
+            let mut current_range = current.1.clone();
+            for action in rules.get(&current.0).unwrap() {
+                println!("{} Action {:?}", current.0, action);
+                let result = action.process_range(&current_range);
+                println!("Result {:?}", result);
+                if result.0 == "R" {
+                    if result.2.is_some() {
+                        current_range = result.2.unwrap().clone();
+                    }
+                    continue;
+                }
+                if result.0 == "A" {
+                    println!("\nFound range!!! action {:?} range: {:?}", action, result.1);
+                    sum += compute_range_possibilities(&result.1);
+                    if result.2.is_some() {
+                        current_range = result.2.unwrap().clone();
+                    }
+                    continue;
+                }
+                next_currents.push((result.0.clone(), result.1.clone())); // Happy path
+                if result.2.is_some() {
+                    current_range = result.2.unwrap().clone();
+                }
+            }
+        }
+        currents = next_currents;
+        if currents.len() == 0 {
+            break;
+        }
+    }
+    println!("Sum {}", sum);
+    println!("Max 256000000000000");
+    println!("She 167409079868000");
+
+    // Test
+    /*
+    X 1 => 1416 | 2662 => 4000    1416 + 1338
+    M 1 => 4000                   4000
+    A 1 => 2006                   2006
+    S 1 => 1351 | 2770 => 4000    1351 + 1230
+    max: 256000000000000
+    res: 167409079868000
+     */
+}
+
 pub fn resolve(input: &String) {
+    let mut rules: HashMap<String, Vec<Action>> = HashMap::new();
+    let mut parse_rules = true;
+    let mut sum = 0;
+    for line in input.lines() {
+        if line.len() == 0 {
+            parse_rules = false;
+            continue;
+        }
+        if parse_rules {
+            let parts: Vec<&str> = line.split("{").map(|x| {
+                x.trim()
+            }).collect();
+            rules.insert(String::from(parts[0]), parts[1][..parts[1].len() - 1].split(",").map(|a| -> Action {
+                return Action::new(a);
+            }).collect());
+            continue;
+        }
+
+        let parts: Vec<usize> = line[1..line.len() - 1].split(",").map(|part| -> usize {
+            return part[2..].parse().unwrap()
+        }).collect();
+        println!("Parts: {:?}", parts);
+        let result = do_workflow(&rules, &parts);
+        println!("do workflow res: {}", result);
+        if (result) {
+            for part in parts {
+                sum += part;
+            }
+        }
+    }
+    println!("Result {}", sum);
+
+    println!("Explore workflows");
+    explore_workflows(&rules);
 
 }
